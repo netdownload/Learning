643cbc6fc030638871942195f997ed3f4b78039f
netdownload <netdownload@yandex.ru>
2022-03-17 23:18:52 +0300

    skillbox

---
+++ Skillbox/02/00_distance.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Есть словарь координат городов
+
+sites = {
+    'Moscow': (550, 370),
+    'London': (510, 510),
+    'Paris': (480, 480),
+}
+
+# Составим словарь словарей расстояний между ними
+# расстояние на координатной сетке - корень из (x1 - x2) ** 2 + (y1 - y2) ** 2
+
+distances = {}
+
+# TODO здесь заполнение словаря
+for _ in (sites.keys()):
+    distances.append((sites.get(_)[0]-sites.get(_)[0])**2+)
+
+print(distances)
+
+
+
+
