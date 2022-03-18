aee73c36527f7cd3bdb474ae2bf0bd8171567e87
netdownload <netdownload@yandex.ru>
2022-03-18 11:58:43 +0300

    my_family

--- Skillbox/02/04_my_family.py
+++ Skillbox/02/04_my_family.py
@@ -20,5 +20,11 @@ my_family_height = [
 
 # Выведите на консоль общий рост вашей семьи как сумму ростов всех членов
 #   Общий рост моей семьи - ХХ см
+family_height = 0
+for _ in range(len(my_family_height)):
+    if my_family_height[_][0] == "Отец":
+        print('Рост отца - '+ str(my_family_height[_][1]) + ' см')
+    family_height = family_height + my_family_height[_][1]
 
+print('Общий рост моей семьи - ' + str(family_height) + ' см')
 print(my_family_height)
