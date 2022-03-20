52be292d05da203a32f3e62fb152a30bd3e6f8ac
netdownload <netdownload@yandex.ru>
2022-03-20 15:23:56 +0300

    oop and fastapi

---
+++ OOP/Udemy/02/python_object.py
@@ -0,0 +1,7 @@
+inty = 5
+listy = [5, 6]
+
+stringy = 'Hi'
+
+import folium
+folium.folium.Map(location = (38, -27), zoom_start = 6).save('output.html')
---
+++ OOP/Udemy/03/point.py
@@ -0,0 +1,41 @@
+class Person:
+    def __init__(self, n, a):
+        self.name = n
+        self.age = a
+
+
+class Point:
+    
+    
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+        
+    def falls_in_rectangle(self, lowleft, upright):
+        if lowleft[0] < self.x < upright[0] and lowleft[1] < self.y < upright[1]:
+            return True
+        else:
+            return False
+        
+    
+    def distance_from_point(self, point):
+        dist = ((self.x - point.x)**2 + (self.y - point.y)**2)**0.5
+        return dist
+    
+
+        
+person1 = Person('John', 25)
+print(person1.name)
+        
+
+
+
+point1 = Point(10, 20)
+point2 = Point(5, 15)
+
+print(point1.falls_in_rectangle((2, 2), (25, 25)))
+print(point1.distance_from_point(point2))
+
+
+        
